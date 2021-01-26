bc505ad17b1aaea377daff129534c1240adf86d8
mahmoud <ma7mod_a@live.com>
2021-01-26 06:36:41 +0200

    added mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,29 @@
 console.log('hello world');
+const body = document.querySelector('body');
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
 btnHamburger.addEventListener('click', e => {
     e.preventDefault;
     console.log('Hamburger button clicked')
 
     if (header.classList.contains('open')){
         header.classList.remove('open')
-        overlay.classList.add('fade-out')
-        overlay.classList.remove('fade-in')
+        fadeElements.forEach( element =>{
+            element.classList.add('fade-out')
+            element.classList.remove('fade-in')
+        })
+        body.classList.remove('no-scroll')
+        
     }else {
         header.classList.add('open')
-        overlay.classList.add('fade-in')
-        overlay.classList.remove('fade-out')
+        fadeElements.forEach( element =>{
+            element.classList.add('fade-in')
+            element.classList.remove('fade-out')
+        })
+        body.classList.add('no-scroll')
     }
     
 })
